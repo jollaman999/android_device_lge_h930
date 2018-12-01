f544af5d7bcd992c4e66629f5073a96c27fd4fb4
SGCMarkus <markusornik@gmail.com>
2018-12-01 19:36:13 +0100

    h930: update fingerprint to H930V21o
    Change-Id: I8155485b6aac20d1b4a53e070eccb370230fa2b5

--- lineage_h930.mk
+++ lineage_h930.mk
@@ -12,8 +12,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="joan" \
     PRODUCT_DEVICE="joan" \
     PRODUCT_NAME="joan_global_com" \
-    PRIVATE_BUILD_DESC="joan_global_com-user 8.0.0 OPR1.170623.026 1819221128e3d release-keys"
+    PRIVATE_BUILD_DESC="joan_global_com-user 8.0.0 OPR1.170623.026 183202318d019 release-keys"
 
-BUILD_FINGERPRINT="lge/joan_global_com/joan:8.0.0/OPR1.170623.026/1819221128e3d:user/release-keys"
+BUILD_FINGERPRINT="lge/joan_global_com/joan:8.0.0/OPR1.170623.026/183202318d019:user/release-keys"
 
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.model ro.vendor.product.model
